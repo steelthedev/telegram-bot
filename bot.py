8cccf404fda6b2bebe496971568f4e44247a13d5
steelthedev <akinwumikaliyanu@gmail.com>
2021-06-21 16:02:42 +0100

    integrated scrape.py

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 import logging
 from telegram.ext import Updater, CommandHandler , MessageHandler , Filters 
+from  scrape import *
 
 def start(update, context):
-    update.message.reply_text("hello! This is man of steel telegram testing")
+    
+    update.message.reply_text(article_name)
 
 def new(update, context):
     update.message.reply_text("Hold on for something new")
-
 
 def main():
     updater=Updater("1786157926:AAENP-1fUdw68NzRoV7z2wxJFNgsk0JnY50",use_context=True)
